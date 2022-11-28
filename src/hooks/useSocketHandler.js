ca292438fb4a4f9593ed42a7dcb9e066a8600d0d
Mohamed Ibrahim <eng.mohammedibrahim904@gmail.com>
2022-11-28 13:18:12 +0200

    bug fixes and partial action addition

--- src/hooks/useSocketHandler.js
+++ src/hooks/useSocketHandler.js
@@ -1,8 +1,5 @@
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
 import ComponentErrorPrompt from "../components/organisms/ComponentErrorPrompt";
 import useLoadingState from "./useLoadingState";
-import { setSocket } from "../slices/general.slice";
 
 export default (socket) => {
   const [_, emitAction, execCMD] = useLoadingState(undefined, socket);
--- src/hooks/useTeacherClass.js
+++ src/hooks/useTeacherClass.js
@@ -1,4 +1,4 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import TeacherClassQuestionTemplate from "../templates/TeacherClassQuestionTemplate";
 import TeacherClassReportsTemplate from "../templates/TeacherClassReportsTemplate";
 import TeacherClassStartTemplate from "../templates/TeacherClassStartTemplate";
